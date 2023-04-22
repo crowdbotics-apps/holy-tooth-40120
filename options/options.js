989ad5bdbbea994d406f597a6a99a42b237d295b
Crowdbotics <team@crowdbotics.com>
2023-04-22 16:35:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-tooth-40120",
+  url: "https://holy-tooth-40120.botics.co",
+  api: "https://holy-tooth-40120.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
